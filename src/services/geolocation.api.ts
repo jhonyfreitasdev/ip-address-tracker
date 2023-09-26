5192e18c8cdc67b243444ffe6c5d8e95bc3eeadf
Jhony Freitas <jhony00._@hotmail.com>
2023-09-26 18:47:17 -0300

    Atualizando chave de acesso da api

--- src/services/geolocation.api.ts
+++ src/services/geolocation.api.ts
@@ -1,6 +1,6 @@
-export const getGeolocation = async (/*country: string*/) => {
-    // const urlCountry = `country?${country}`
-    const response= await fetch(`https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_7aAE9kny0QpfuZPgLXcInG7n009GV&ipAddress=177.152.137.6`)
+export const getGeolocation = async () => {
+        
+    const response= await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_3wiTGmcvqMJDMnmtzcZhhWvXXAiIF&ipAddress=179.250.148.78`)
     
     return await response.json()
 }
