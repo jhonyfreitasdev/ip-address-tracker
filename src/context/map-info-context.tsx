c6e689ec508c1f57579eb5573537eaf6da51d581
Jhony Freitas <jhony00._@hotmail.com>
2023-09-27 19:35:55 -0300

    Criando context com as informações do mapa

---
+++ src/context/map-info-context.tsx
@@ -0,0 +1,32 @@
+import { createContext, useState } from "react"
+
+import { MapInfoTypes } from "../types/map-info";
+import { ProviderProps } from "../types/provider-props";
+
+const defaultObject: MapInfoTypes = {
+    ip: 0,
+    lat: 0,
+    lng: 0,
+    location: "",
+    timezone: ""
+}   
+
+type MapContextType = {
+    mapInfo: MapInfoTypes | undefined;
+    setMapInfo: (info: MapInfoTypes | undefined) => void;
+}
+
+export const MapContext = createContext<MapContextType>({
+    mapInfo: defaultObject,
+    setMapInfo: () => {} 
+})
+
+export const MapProvider: React.FC<ProviderProps> = ({ children }) => {
+    const [mapInfo, setMapInfo] = useState<MapInfoTypes>()
+
+    return (
+        <MapContext.Provider value={{ mapInfo, setMapInfo }}>
+            {children}
+        </MapContext.Provider>
+    )
+}  
